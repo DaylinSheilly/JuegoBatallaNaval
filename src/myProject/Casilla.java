2075cc021abcd97aa7e89f4116b228108fdcc1a9
Sheilly Ortega 2040051-3743 <sheilly.ortega@correounivalle.edu.co>
2022-03-08 11:21:01 -0500

    Writing Casilla class. Part 1

--- src/myProject/Casilla.java
+++ src/myProject/Casilla.java
@@ -1,4 +1,40 @@
 package myProject;
 
-public class Casilla {
+import javax.swing.*;
+import java.awt.*;
+
+public class Casilla extends JButton {
+    int fila, columna, nombreDelBarco;
+    boolean parteDelBarco;
+    private ImageIcon imageBarco, imagenNuevoTamanho;
+    private Image imagenOtroTamanho;
+
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        g.setColor(new Color(20, 90, 219));
+        g.drawRect(fila, columna, 5, 5);
+
+    }
+
+    public int getBarco(Casilla casilla){
+        return  nombreDelBarco;
+
+    }
+
+    public boolean getParteDelBarco(Casilla casilla){
+        return parteDelBarco;
+    }
+
+    /*public void insertarBarco(int nombreDelBarco){
+        if(nombreDelBarco == 1){
+            parteDelBarco=false;
+            barco.insertarTodoElBarco;
+        }
+        if(nombreDelBarco == 2){
+            parteDelBarco = true;
+            barco.insertarTodoElBarco;
+        }
+    }*/
 }

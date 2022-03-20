79a83d6381c581b3b4b287654a436756e869d72e
202043008 Juan Mazuera <juan.yunda@correounivalle.edu.co>
2022-03-20 08:13:41 -0500

    created the user boats and its resistence

--- src/myProject/ModelBatallaNaval.java
+++ src/myProject/ModelBatallaNaval.java
@@ -1,12 +1,19 @@
 package myProject;
 
-import java.util.Vector;
+import java.util.ArrayList;
 
 public class ModelBatallaNaval {
     private CasillaPosicion parte1;
-    private int fila, columna, puntos;
+    private int fila, columna, puntos,
+            fragataPosicion1Resistencia, fragataPosicion2Resistencia, fragataPosicion3Resistencia, fragataPosicion4Resistencia, destructorPosicion1Resistencia,
+            destructorPosicion2Resistencia, destructorPosicion3Resistencia, submarinoPosicion1Resistencia, submarinoPosicion2Resistencia, portavionPosicionResistencia,
+            fragataPrincipal1Resistencia, fragataPrincipal2Resistencia, fragataPrincipal3Resistencia, fragataPrincipal4Resistencia, destructorPrincipal1Resistencia,
+            destructorPrincipal2Resistencia, destructorPrincipal3Resistencia, submarinoPrincipal1Resistencia, submarinoPrincipal2Resistencia, portavionPrincipalResistencia;
     private boolean ganar;
-    private Vector<CasillaPrincipal> barco;
+    private ArrayList<CasillaPosicion> fragataPosicion1, fragataPosicion2, fragataPosicion3, fragataPosicion4, destructorPosicion1, destructorPosicion2,
+            destructorPosicion3, submarinoPosicion1, submarinoPosicion2, portavionPosicion;
+    private ArrayList<CasillaPrincipal> fragataPrincipal1, fragataPrincipal2, fragataPrincipal3, fragataPrincipal4, destructorPrincipal1, destructorPrincipal2,
+            destructorPrincipal3, submarinoPrincipal1, submarinoPrincipal2, portavionPrincipal;
 
 
     public ModelBatallaNaval(){
@@ -41,6 +48,11 @@ public class ModelBatallaNaval {
         parte3.setTieneBarco(true);
         parte4.pintarParteDelBarco("portaaviones");
         parte4.setTieneBarco(true);
+        portavionPosicion.add(parte1);
+        portavionPosicion.add(parte2);
+        portavionPosicion.add(parte3);
+        portavionPosicion.add(parte4);
+        portavionPosicionResistencia = 4;
     }
 
     public void casillasDelBote(CasillaPosicion parte1, CasillaPosicion parte2,
@@ -51,6 +63,18 @@ public class ModelBatallaNaval {
         parte2.setTieneBarco(true);
         parte3.pintarParteDelBarco("submarino");
         parte3.setTieneBarco(true);
+        if(submarinoPosicion1.size() == 0){
+            submarinoPosicion1.add(parte1);
+            submarinoPosicion1.add(parte2);
+            submarinoPosicion1.add(parte3);
+            submarinoPosicion1Resistencia = 3;
+        }
+        else if(submarinoPosicion2.size() == 0){
+            submarinoPosicion2.add(parte1);
+            submarinoPosicion2.add(parte2);
+            submarinoPosicion2.add(parte3);
+            submarinoPosicion2Resistencia = 3;
+        }
     }
 
     public void casillasDelBote(CasillaPosicion parte1, CasillaPosicion parte2){
@@ -58,11 +82,42 @@ public class ModelBatallaNaval {
         parte1.setTieneBarco(true);
         parte2.pintarParteDelBarco("destructor");
         parte2.setTieneBarco(true);
+        if(destructorPosicion1.size() == 0){
+            destructorPosicion1.add(parte1);
+            destructorPosicion1.add(parte2);
+            destructorPosicion1Resistencia = 2;
+        }
+        else if(destructorPosicion2.size() == 0){
+            destructorPosicion2.add(parte1);
+            destructorPosicion2.add(parte2);
+            destructorPosicion2Resistencia = 2;
+        }
+        else if(destructorPosicion3.size() == 0){
+            destructorPosicion3.add(parte1);
+            destructorPosicion3.add(parte2);
+            destructorPosicion3Resistencia = 2;
+        }
     }
 
     public void casillasDelBote(CasillaPosicion parte1){
         parte1.pintarParteDelBarco("fragata");
         parte1.setTieneBarco(true);
+        if(fragataPosicion1.size() == 0){
+            fragataPosicion1.add(parte1);
+            fragataPosicion1Resistencia = 1;
+        }
+        else if(fragataPosicion2.size() == 0){
+            fragataPosicion2.add(parte1);
+            fragataPosicion2Resistencia = 1;
+        }
+        else if(fragataPosicion3.size() == 0){
+            fragataPosicion3.add(parte1);
+            fragataPosicion3Resistencia = 1;
+        }
+        else if(fragataPosicion4.size() == 0){
+            fragataPosicion4.add(parte1);
+            fragataPosicion4Resistencia = 1;
+        }
     }
 
     public void determinarGanador(){
